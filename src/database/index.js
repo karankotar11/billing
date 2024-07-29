d45e0d11b19a467003ed4ccbb0dd3093e9cc3d46
karankotar11 <karankotar11@gmail.com>
2024-07-29 20:17:28 +0530

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 const connectToDB= async()=>{
-	const url="mongodb://localhost:27017/userdatabasenext"
+	const url="mongodb+srv://karankotar11:67B0dLWuu027xfTU@karan.ezthbnv.mongodb.net/"
 	mongoose.connect(url)
 	.then(()=>console.log("database connection is successfull"))
 	.catch((e)=>console.log(e));
