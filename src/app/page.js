2d1040c3c42b570ae1946ec87f4a9398569e69a8
karankotar11 <karankotar11@gmail.com>
2024-07-29 17:09:35 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 
 'use client'
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { initialBillData } from "./utils";
 import GetFormData from "./data-form";
 import PrintData from "./print-data/page";
@@ -14,7 +14,31 @@ export default function Home() {
 
   const [displayform, setdisplayform] = useState('');
   const [displaydata, setdisplaydata] = useState('hidden')
-  const [billData, setbillData] = useState(initialBillData)
+  // const [billData, setbillData] = useState(JSON.parse(localStorage.getItem('billdata')) || initialBillData)
+  const [billData, setbillData] = useState(() => {
+    if (typeof window !== 'undefined') {
+      const storedBillData = localStorage.getItem('billdata');
+      return storedBillData ? JSON.parse(storedBillData) : initialBillData;
+    }
+    return initialBillData;
+  });
+
+  useEffect(() => {
+    
+    localStorage.setItem('billdata',JSON.stringify(billData))
+  }, [billData])
+  
+  // useEffect(() => {
+  //   if (typeof window !== 'undefined') {
+  //     const storedBillData = localStorage.getItem('billdata');
+  //     if (storedBillData) {
+  //       setbillData(JSON.parse(storedBillData));
+  //     }
+  //   }
+  // }, [])
+  
+
+  
 
 
   return (
