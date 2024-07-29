99e365cbb15d9f4f4dedb6aee431367f5988a42c
karankotar11 <karankotar11@gmail.com>
2024-07-29 20:50:54 +0530

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,14 @@
-'use server'
-
 import connectToDB from "@/database";
 import LocationofUser from "@/model"
 
-export async function SaveLocation(location){
+export async function SaveLocation(location) {
     await connectToDB();
-    try{
-         LocationofUser.create(location)
-       
-    
-    }
-    catch(e){
-        console.log(e);
+
+    try {
+        const createdLocation = await LocationofUser.create(location);
+        return { success: true, data: createdLocation }; // Return success and created document
+    } catch (e) {
+        console.error(e);
+        return { success: false, error: e.message }; // Return error message
     }
 }
