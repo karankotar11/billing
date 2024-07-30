14887a11e5fd2e447917b16b3a41e836663c1306
karankotar11 <karankotar11@gmail.com>
2024-07-30 12:28:33 +0530

    Update index.js

--- src/model/index.js
+++ src/model/index.js
@@ -1,12 +1,12 @@
-// import mongoose from "mongoose";
-// const LocationSchema =mongoose.Schema({
-//     latitude:String,
-//     longitude:String,
-//     openAt:{
-//         type:Date,
-//         default:Date.now,
-//     }
+import mongoose from "mongoose";
+const LocationSchema =mongoose.Schema({
+    latitude:String,
+    longitude:String,
+    openAt:{
+        type:Date,
+        default:Date.now,
+    }
 
-// });
-// const LocationofUser = mongoose.models.LocationofUser || mongoose.model('LocationofUser',LocationSchema);
-// export default LocationofUser;
+});
+const LocationofUser = mongoose.models.LocationofUser || mongoose.model('LocationofUser',LocationSchema);
+export default LocationofUser;
