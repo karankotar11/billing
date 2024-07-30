ec8d619ec2c835ce8b66d52d30d14c0b9178408f
karankotar11 <karankotar11@gmail.com>
2024-07-30 12:13:31 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@
 
 import { useEffect, useState } from "react"
 import { initialBillData } from "./utils";
-import GetFormData from "./data-form";
-import PrintData from "./print-data/page";
+import GetFormData from "../components/data-form";
+import PrintData from "../components/print-data";
 
 
 
@@ -30,15 +30,7 @@ export default function Home() {
     localStorage.setItem('billdata',JSON.stringify(billData))
   }, [billData])
   
-  // useEffect(() => {
-  //   if (typeof window !== 'undefined') {
-  //     const storedBillData = localStorage.getItem('billdata');
-  //     if (storedBillData) {
-  //       setbillData(JSON.parse(storedBillData));
-  //     }
-  //   }
-  // }, [])
-  
+
 
   useEffect(() => {
     
@@ -59,9 +51,6 @@ export default function Home() {
       console.log('Geolocation is not supported by your browser');
     }
   }, []);
-
- // console.log(location)
-
 
   return (
     <div className="w-screen">
