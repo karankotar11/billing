9b437b6684c5d695292dc3f3a3b8c15f471d3a60
karankotar11 <karankotar11@gmail.com>
2024-07-30 12:18:32 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output:"export",
+    output:"export",
     // experimental: {
     //     serverActions: true, // Enable server actions if you're using them
     //   },
